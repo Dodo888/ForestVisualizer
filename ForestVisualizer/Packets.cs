7d56b938d10bb9e0302289c87abd3dd1997b0e9a
Dodo888 <darkdodo888@gmail.com>
2015-05-19 22:41:02 +0500

    Grand Visualizer Update
    Тестовая версия для проверки работоспособности

---
+++ ForestVisualizer/Packets.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForestVisualizer
+{
+    public class Hello
+    {
+        public bool IsVisualizator;
+        public string Name;
+    }
+
+    public class WorldInfo
+    {
+        public Player[] Players;
+        public int[,] Map;
+    }
+
+    public class Answer
+    {
+        public int AnswerCode;
+    }
+
+    public class LastMoveInfo
+    {
+        public bool GameOver;
+        public Tuple<Point, int>[] ChangedCells;
+        public Tuple<int, Point, int>[] PlayersChangedPosition; // <id, new position, new hp>
+    }
+}
